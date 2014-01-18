fd3827f317bcb470a0bce89711c02192cce3cc0e
Mike Flynn <mflynn@cngann.com>
2014-01-18 11:37:44 -0500

    App Icon  and Launch Images

--- Clienteling/Clienteling/AppDelegate.m
+++ Clienteling/Clienteling/AppDelegate.m
@@ -16,6 +16,16 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+
+    NSString *url = @"http://mflynn:8080/Clienteling/index";
+    
+    // Open with Google Chrome if possible
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL   URLWithString:@"googlechrome:"]]) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"googlechrome://%@", url]]];
+    } else {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"itms-apps://itunes.apple.com/us/app/chrome/id535886823"]];
+    }
+    
     return YES;
 }
 
