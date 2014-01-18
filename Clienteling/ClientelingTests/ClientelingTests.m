59795528da4de3a37aadf633b350353d65f0d818
Mike Flynn <mflynn@cngann.com>
2014-01-18 10:14:31 -0500

    Initial Commit

---
+++ Clienteling/ClientelingTests/ClientelingTests.m
@@ -0,0 +1,34 @@
+//
+//  ClientelingTests.m
+//  ClientelingTests
+//
+//  Created by Michael Flynn on 1/18/14.
+//  Copyright (c) 2014 Michael Flynn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClientelingTests : XCTestCase
+
+@end
+
+@implementation ClientelingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
